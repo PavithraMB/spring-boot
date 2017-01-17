4151aae6d6dd2fc4dc4b02428b9253b4f6d9d7dc
PavithraMB <pavithra.mankompubalaji@gmail.com>
2017-01-17 11:48:37 -0500

    Registration application sample in Spring Boot

---
+++ SpringBootRegistration/src/main/java/com/example/registration/api/RegistrationController.java
@@ -0,0 +1,18 @@
+package com.example.registration.api;
+
+import java.util.Collection;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegistrationController {
+	 @RequestMapping("/")
+	    public String index() {
+	        return "Hello World!";
+	    }
+}
